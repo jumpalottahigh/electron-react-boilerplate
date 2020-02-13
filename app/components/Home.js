fbd0d7b7d120dc35528e593b96fa2f8d27866afc
Georgi Yanev <georgiyanev.gy@gmail.com>
2020-02-13 09:35:15 +0200

    Test sharp building

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,32 +5,36 @@ import sharp from 'sharp';
 import routes from '../constants/routes.json';
 import styles from './Home.css';
 
-// type Props = {};
-
-// const imagePath =
-//   '/Users/georgiyanev/Projects/FEEDBACK FROM ANTTI/IMAGES-ARE-MISSING-THUMBNAILS/P130351/C73_P103351_G-1.JPG';
+const imagePath =
+  '/Users/georgiyanev/Projects/FEEDBACK FROM ANTTI/SOLVED/MANY WORKING SETS/C246_P272378_SS/246_P272378_G-2.JPG';
 
 const Home = () => {
-  // const [counter, setCounter] = React.useState('');
+  const [image, setImageData] = React.useState({});
 
-  // React.useEffect(() => {
-  //   const image = sharp(imagePath)
-  //     .metadata()
-  //     .then(data => {
-  //       console.log(data);
-  //       setCounter(data.format);
-  //       return data;
-  //     });
-
-  //   console.log(image);
-  // }, []);
+  React.useEffect(() => {
+    try {
+      // eslint-disable-next-line promise/catch-or-return
+      sharp(imagePath)
+        .metadata()
+        .then(data => {
+          console.log(data);
+          setImageData(data);
+          return data;
+        });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }, []);
 
   return (
     <div className={styles.container} data-tid="container">
       <h2>Home</h2>
       <Link to={routes.COUNTER}>to Counter</Link>
-      {/* <p>{counter}</p> */}
       <p>{sharp ? 'Sharp is running' : 'Sharp is not running'}</p>
+      <div>Reading image: {imagePath}</div>
+      <div>Image format: {image.format}</div>
+      <div>Image width: {image.width}</div>
+      <div>Image height: {image.height}</div>
     </div>
   );
 };
